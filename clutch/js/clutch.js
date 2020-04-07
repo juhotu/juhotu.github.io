5023b8447f900b9b9a0ae41a2d864b1bfc0b7311
JuhoTu <54879860+JuhoTu@users.noreply.github.com>
2020-04-07 15:30:05 +0300

    responsive nav
    it took too long but now is working. deleted different script page as it was causing issues

--- clutch/js/clutch.js
+++
@@ -1,7 +0,0 @@
-function date() {
-    let dateObj = new Date();
-    let today = dateObj.getUTCDate();
-    let difference = 24;
-    difference = difference - today;
-    document.getElementById("dateDiff").innerHTML = "ENÄÄ " + difference + " PÄIVÄÄ CLUTCHIIN";
-}
